eea41c9fbc897cbee1bb0126cdfd513e557da511
Pravin Suresh <selfstudy210619@gmail.com>
2025-10-25 20:18:19 +0530

    infinite scroll with intersectionObserver

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-
+import { useState, useRef, useCallback } from "react";
+import useBookSearch from "./hooks/useBookSearch";
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const {books, hasMore, loading, error} = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback((node)=>{
+    if (loading) return;
+    if (observer.current) {observer.current.disconnect()};//to disconnect from previous last element when newly scrolled down
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore){
+        setPageNumber(prevPage => prevPage + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+    console.log('node', node);
+  }, [loading, hasMore]);
+
+
   return (
     <div className="App">
+      <input type="text" value={query} onChange={(e)=>{setQuery(e.target.value); setPageNumber(1)}} />
+      {books.map((book, index) => {
+        if(index === books.length-1){
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        } else{
+          return <div key={book}>{book}</div>
+        }
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error...'}</div>
     </div>
   );
 }
